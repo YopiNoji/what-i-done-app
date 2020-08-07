bb6075ee44be871dbcb55c3abcd75606f3f5fcab
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-08-07 21:43:29 +0900

    add: twitter component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import AnimatedAppLoader from './components/AnimatedAppLoader'
+import Twitter from './components/Twitter'
 import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <AnimatedAppLoader image={{ uri: Constants.manifest.splash.image }}>
       <SafeAreaView style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <Twitter></Twitter>
       </SafeAreaView>
     </AnimatedAppLoader>
   );
