d6136073484c88a18cb4feb1c14681172a6b21a4
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-08-07 14:53:42 +0900

    add: meta info to app.config.js

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
 export default {
+    name: "What I Done App",
+    description: "An app to record and reflect on what i done.",
     icon:
       "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/icon.png?raw=true",
     splash: {
@@ -7,4 +9,9 @@ export default {
       resizeMode: "contain",
       backgroundColor: "#ffffff",
     },
+    web:{
+      lang: "en",
+      name: "What I Done App",
+      description: "An app to record and reflect on what i done.",
+    }
 };
