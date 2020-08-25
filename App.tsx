b1e2510f55692271915a1abeccc28041ad7c84e5
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-08-25 23:46:36 +0900

    add Firebase auth & add React Navigation

--- App.tsx
+++ App.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import AnimatedAppLoader from './components/AnimatedAppLoader'
-import Tasks from './components/Tasks'
-import LoginWithAuth0 from './components/LoginWithAuth0'
+// import Tasks from './components/Tasks'
+// import LoginWithAuth0 from './components/LoginWithAuth0'
 import Constants from 'expo-constants';
 import { ApolloProvider } from '@apollo/client';
 import { apolloClient } from './apollo';
+import Providers from './navigation/Providers';
 
 export default function App() {
   return (
     <AnimatedAppLoader image={{ uri: Constants.manifest.splash.image }}>
-      <SafeAreaView style={styles.container}>
         <ApolloProvider client={apolloClient}>
-          <Tasks></Tasks>
-          <LoginWithAuth0></LoginWithAuth0>
+          <Providers />
         </ApolloProvider>
-      </SafeAreaView>
     </AnimatedAppLoader>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Firebase/firebaseConfig.ts
@@ -0,0 +1,13 @@
+// Rename this file to "firebaseConfig.js" before use
+// Replace all XXXXs with real Firebase API keys
+
+export default {
+  apiKey: "AIzaSyCTvgp_NqFLxMxLp7qoX0K9EnTRy1iAx-I",
+  authDomain: "what-i-done-app.firebaseapp.com",
+  databaseURL: "https://what-i-done-app.firebaseio.com",
+  projectId: "what-i-done-app",
+  storageBucket: "what-i-done-app.appspot.com",
+  messagingSenderId: "182998114971",
+  appId: "1:182998114971:web:0ecbf72e6873bb8c6e9c3e",
+  measurementId: "G-JHDFSN0DTG"
+};
